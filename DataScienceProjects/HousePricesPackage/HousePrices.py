4b481873b416d5c5288414f8f44f42f15ab13a36
aj <anthonyjefferson@web.de>
2023-04-30 17:25:19 +0200

    drop method to get rid of columns with null-values > 0.5

--- DataScienceProjects/HousePricesPackage/HousePrices.py
+++ DataScienceProjects/HousePricesPackage/HousePrices.py
@@ -1,26 +1,30 @@
 
 #importing the libraries
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#for demonstrational purpose to print n lines where n<=3000
+#Code found on Stackoverflow
+pd.options.display.width = None
+pd.options.display.max_columns = None
+pd.set_option('display.max_rows', 3000)
+pd.set_option('display.max_columns', 3000)
+
 
 #importing the Dataset
 train_data = pd.read_csv("/Users/anthonyjefferson/PycharmProjects/"
                          "DataScience_Jefferson/house-prices-advanced-regression-techniques/"
                          "train.csv")
+print(train_data.head())
 
 test_data = pd.read_csv("/Users/anthonyjefferson/PycharmProjects/"
                         "DataScience_Jefferson/house-prices-advanced-regression-techniques/"
                         "test.csv")
+print(test_data.head())
 
-X_train = train_data.iloc[:, :-1].values
-y = train_data.iloc[:, -1].values
+print(train_data.isnull().sum())
+print(sns.heatmap(train_data.isnull(), yticklabels=False,cbar=False))
 
-#importing the xgbClassifier to fit the data
-from xgboost import XGBClassifier
-model = XGBClassifier()
-model.fit(X_train, y)
-
-
-
-
+train_data = train_data.drop(columns=['Alley', 'MasVnrType', 'FireplaceQu', 'PoolQC', 'Fence', 'MiscFeature'])
+print(train_data.isnull().sum())

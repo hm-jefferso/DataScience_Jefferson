e7a289d6211ad6d741e8ea3d6c85b629b7ded598
aj <anthonyjefferson@web.de>
2023-05-01 16:58:02 +0200

    - removed y_pred -added singular test set result based off Lvl. 5.5 - corrected iloc Functions -added .values -removed accuracy_score

--- DataScienceProjects/PositionSalaries/PositionSalariesRFR.py
+++ DataScienceProjects/PositionSalaries/PositionSalariesRFR.py
@@ -1,33 +1,35 @@
 #importing libraries
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 ##importing dataset
 dataset = pd.read_csv('/Users/anthonyjefferson/PycharmProjects/DataScience_Jefferson/DataScienceProjects/PositionSalaries/Position_Salaries-2.csv')
 print(dataset.head())
 
 #definining Features X and dependent variable y
-X = dataset.iloc[:, -1]
-y = dataset.iloc[:, :-1]
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
 
 #splitting dataset into training and testing data
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(test_size=0.3, train_size=0.7, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-#Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
+plt.scatter(X,y)
+plt.show()
 
 #RandomForestRegressor
 from sklearn.ensemble import RandomForestRegressor
-rfr = RandomForestRegressor()
+rfr = RandomForestRegressor(n_estimators= 20, random_state=42)
 rfr.fit(X, y)
+
+#predicting singular result
+example = rfr.predict([[5.5]])
+print(f'at 5.5 the estameted salary is: {example}')
 
 #predicting test set results
 y_pred = rfr.predict(X_test)
-print(y_pred)
+print(f'predicted values: {y_pred}')
 
-#checking model for accuracy and
-from sklearn.metrics import accuracy_score
-acs = accuracy_score(y_pred)
-print(acs)
+
+
